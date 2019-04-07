0b9edcde704545142d45f6e075c2c5e2a9af3e89
odayabueid <odayabueid1992m@gmail.com>
2019-04-07 14:19:03 +0300

    solution for recursion

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,8 +1,75 @@
-// this is what you would do if you liked things to be easy:
+ // this is what you would do if you liked things to be easy:
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
+var stringifyJSON = function(obj)  {
   // your code goes here
-};
+	var str = "";
+
+	if(obj === undefined || typeof obj ==='function'){
+
+		return "";
+	}else if(obj === null){
+		str = "null"
+	}else{
+
+		if(Array.isArray(obj)){
+
+			str += "["
+
+				for(var k in obj){
+
+					str += stringifyJSON(obj[k]);
+
+					if(k<obj.length-1){
+
+					str+= ","
+					}
+				}
+
+				
+
+			str += "]"
+		}else if(typeof obj !== 'object'){
+
+			if(typeof obj === "string"){
+
+				str += "\"" + obj + "\"";
+			}else{
+
+				str += obj;
+			}
+
+		}else{
+
+			str += "{"
+
+				for(var k in obj){
+
+					if(k === 'undefined' || k === 'functions'){
+						str+= "";
+					}else{
+
+						str += "\"" + k + "\":" + stringifyJSON(obj[k]) 
+
+						if(k != Object.keys(obj)[Object.keys(obj).length-1]){
+
+							str += ','
+						}
+					}
+				}
+
+			str += "}"
+
+		}
+
+
+	}
+
+	return str;
+
+ }
+
+
+
